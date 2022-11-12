6a3e2337e64e3ffb98766f493c17fcbaf1eed2df
Sapfir0 <sapfir999999@yandex.ru>
2022-11-12 19:31:33 +0300

    Moved pages to separated folders

--- src/pages/Overview/Overview.tsx
+++ src/pages/Overview/Overview.tsx
@@ -4,6 +4,7 @@ import styles from './Overview.module.scss'
 
 export function Overview() {
     return <><h1>Overview</h1>
+    <div className={styles.root}>
     <Card header={<>Camera</>}><>f</></Card>
     <Card header={<>Status by units</>}><Statement value={22} dimension='мм' size='large'/></Card>
     <Card header={<>Outside</>}><Statement value={20.0} dimension='F' size='large'/></Card>
@@ -19,5 +20,6 @@ export function Overview() {
             <Statement value={23.0} dimension='F' size='large' />
             <Statement value={23.0} dimension='F' size='large' />
         </div></Card>
+        </div>
     </>
 }
